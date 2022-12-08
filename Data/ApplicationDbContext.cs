77e90abfd546e3795f685198be7333a7170e6395
Ben <78624890+BenjaminG182@users.noreply.github.com>
2022-12-07 17:31:26 -0700

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BenJoke.Models;
+
+namespace BenJoke.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BenJoke.Models.Joke> Joke { get; set; }
+    }
+}
